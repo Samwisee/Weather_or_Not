17007c9f4f81dbf176d170f670216c5147a18b5a
Harris Rahim <harris.rhm@gmail.com>
2020-09-14 17:28:04 +1000

    FEAT: default devise  edit page is changed to our edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+        redirect_to user_path(@user)
+    else
+        render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :city_name, :latitude, :longtitude, :email)
+  end
+
 end
