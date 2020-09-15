a4a212aaf028de772378c12280d8557d2aebf9ac
Sam Cheney <samcheneyus@gmail.com>
2020-09-15 15:46:42 +1000

    form with start_at and end_at as :time types

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_14_035631) do
+ActiveRecord::Schema.define(version: 2020_09_14_035010) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "freetimes", force: :cascade do |t|
-    t.string "start_at"
-    t.string "end_at"
+    t.time "start_at"
+    t.time "end_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
