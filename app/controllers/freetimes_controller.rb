d8e2c6b75ef39d6ad4839ed129cc49d447232c2a
Sam Cheney <samcheneyus@gmail.com>
2020-09-15 11:15:29 +1000

    Set up freetimes controller

--- app/controllers/freetimes_controller.rb
+++ app/controllers/freetimes_controller.rb
@@ -4,7 +4,19 @@ class FreetimesController < ApplicationController
   end
 
   def create
-
+    @freetime = Freetime.new(freetime_params)
+    @freetime.user = current_user
+    if @freetime.save
+      redirect_to 
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def human_params
+    params.require(:freetime).permit(:start_at, :end_at)
+  end
+ 
 end
