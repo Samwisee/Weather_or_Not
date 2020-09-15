85039e8ca55b812430e30e83802c5906ff7dbcfb
Sam Cheney <samcheneyus@gmail.com>
2020-09-15 17:32:42 +1000

    got the sign_in path to work...no idea why

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   get '/rails/info/routes' => 'routes'
+  get 'devise/registrations/after_signup'
 
   resources :freetimes
   resources :users
