2284351e428ee90244bb08d77eb7171a88e3ed13
Harris Rahim <harris.rhm@gmail.com>
2020-09-15 12:07:32 +1000

    updated

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,12 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  # def show
+  #   @user = User.find(params[:id])
+  # end
+
   def update
+    @user = User.find(params[:id])
     if @user.update(user_params)
         redirect_to user_path(@user)
     else
