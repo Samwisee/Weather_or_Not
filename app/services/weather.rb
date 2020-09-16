be3fe7ce9be0b09eedb9f16f1f124a47d3bdbade
Samwise <samcheneyus@gmail.com>
2020-09-16 11:18:29 +1000

    Update weather.rb

--- app/services/weather.rb
+++ app/services/weather.rb
@@ -6,9 +6,9 @@ class Weather
         api_key= ENV['OPENWEATHER_KEY']
         # url = "https://api.openweathermap.org/data/2.5/weather?q=#{city_name}&exclude=minutely&units=metric&APPID=#{api_key}"
         url= "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely&appid=#{api_key}"
-        user_serialized = open(url).read
-        user = JSON.parse(user_serialized)
-        return user
+        forecast_serialized = open(url).read
+        forecast = JSON.parse(forecast_serialized)
+        return forecast
         # return "#{user['name']} #{user['main']['temp'].round}c "
     end
 end
