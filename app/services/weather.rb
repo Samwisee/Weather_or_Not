38d8e162f0384c86e38f478b6fdf93c90d5d8548
Eric Chen <eric19920108@gmail.com>
2020-09-16 16:41:45 +0800

    filter the data, but still have logic problem for the best time to go out

--- app/services/weather.rb
+++ app/services/weather.rb
@@ -4,12 +4,16 @@ require 'open-uri'
 class Weather
     def self.call(lat, lon)
         api_key= ENV['OPENWEATHER_KEY']
-        # url = "https://api.openweathermap.org/data/2.5/weather?q=#{city_name}&exclude=minutely&units=metric&APPID=#{api_key}"
-        url= "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely&appid=#{api_key}"
+        url= "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{lon}&exclude=minutely&units=metric&appid=#{api_key}"
         forecast_serialized = open(url).read
         forecast = JSON.parse(forecast_serialized)
+        # store in json and read it
+        # File.open('./weather_data.json', 'wb') do |file|
+        #     file.write(JSON.generate(forecast))
+        # end
+        # weather_data = File.read('./weather_data.json')
+        # return weather_data
         return forecast
-        # return "#{user['name']} #{user['main']['temp'].round}c "
     end
 end
 
