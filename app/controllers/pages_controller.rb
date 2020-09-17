08bd6fea6effe19cb5e2437293932a124b707673
Harris Rahim <harris.rhm@gmail.com>
2020-09-17 15:25:46 +1000

    FEAT: completed with the weather logic to show the next day best time to go out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,11 +2,10 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-    name = current_user.name
-    lat = current_user.latitude
-    lon = current_user.longitude
-    data = Weather.call(lat, lon)
-    @forecast = Forecast.new(data)
+    # lat = current_user.latitude
+    # lon = current_user.longitude
+    # data = Weather.call(lat, lon)
+    # @forecast = Forecast.new(data)
   end
 
 end
