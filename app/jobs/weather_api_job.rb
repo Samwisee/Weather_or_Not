9fad352b35f8f3c8bfda9a6737a9a87161e45416
Harris Rahim <harris.rhm@gmail.com>
2020-09-18 12:05:01 +1000

    FEAT: WeatherApiJob perfomr method updated

--- app/jobs/weather_api_job.rb
+++ app/jobs/weather_api_job.rb
@@ -5,8 +5,12 @@ class WeatherApiJob < ApplicationJob
     # Do something later
     puts "I'm starting the weather API job"
     # We need to update for all user # for each user
-    user = User.last
-    updated_weather = Weather.call(user.latitude, user.longitude)
+    # user = User.last
+    # updated_weather = Weather.call(user.latitude, user.longitude)
+    User.all.each do |user|
+      Weather.call.update(user.latitude, user.longitude)
+    end
+    
     puts updated_weather
     puts "Weather has been refreshed"
   end
