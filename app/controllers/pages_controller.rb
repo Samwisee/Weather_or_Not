3d6c96a9e684932055b1f0779bd8b5558ba6ddd4
Eric Chen <eric19920108@gmail.com>
2020-09-18 09:44:55 +0800

    Finish the api and the function for best time to go out

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,8 +2,12 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
-      
+    # lat = current_user.latitude
+    # lon = current_user.longitude
+    # data = Weather.call(lat, lon)
+    # @forecast = Forecast.new(data)
   end
 
+  
   
 end
--- app/services/convert_city.rb
+++
@@ -1,8 +0,0 @@
-class ConvertCity
-    def self.call
-        lat = current_user.latitude
-        lon = current_user.longitude
-        data = Weather.call(lat, lon)
-        @forecast = Forecast.new(data)
-    end
-end
