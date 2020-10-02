4f7756d8cde62050910e572e0bb08338c74585a0
Sam Cheney <samcheneyus@gmail.com>
2020-10-02 15:15:00 +1000

    about to do heroku stuff

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,8 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
   
-  
   def home
-    if current_user
+    if current_user # TODO this is breaking sign out 
       lat = current_user.latitude
       lon = current_user.longitude
       data = Weather.call(lat, lon)
